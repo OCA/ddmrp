20ff2c7bea006525ff37333bd755d11837a02558
jbeficent <jordi.ballester@eficent.com>
2016-10-25 13:53:32 +0200

    [IMP] split demand estimate to a separate module

--- ddmrp/models/__init__.py
+++ ddmrp/models/__init__.py
@@ -5,8 +5,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import stock_buffer_profile
-from . import stock_buffer_demand_estimate_period
-from . import stock_buffer_demand_estimate
 from . import procurement_order
 from . import product_adu_calculation_method
 from . import stock_warehouse_orderpoint
