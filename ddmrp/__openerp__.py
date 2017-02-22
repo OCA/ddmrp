5b4f03420ca480562d9e2613e98d786d2762533a
Jordi Ballester <jordi.ballester@eficent.com>
2017-02-22 15:36:28 +0100

    Migration to v9

--- ddmrp/__openerp__.py
+++ ddmrp/__openerp__.py
@@ -7,8 +7,8 @@
 {
     "name": "DDMRP",
     "summary": "Demand-driven MRP",
-    "version": "8.0.1.0.0",
-    "author": "Eficent Business and IT Consulting Services S.L,"
+    "version": "9.0.2.0.0",
+    "author": "Eficent,"
               "Aleph Objects, Inc.,"
               "Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
@@ -17,19 +17,25 @@
                 "mrp",
                 "web_tree_dynamic_colored_field",
                 "stock_warehouse_orderpoint_stock_info",
+                "stock_warehouse_orderpoint_stock_info_unreserved",
                 "stock_available_unreserved",
                 "stock_orderpoint_uom",
                 "stock_orderpoint_manual_procurement",
                 "stock_demand_estimate"],
     "data": ["data/product_adu_calculation_method_data.xml",
+             "data/stock_buffer_profile_variability_data.xml",
+             "data/stock_buffer_profile_lead_time_data.xml",
              "data/stock_buffer_profile_data.xml",
              "security/ir.model.access.csv",
              "security/stock_security.xml",
+             "views/stock_buffer_profile_variability_view.xml",
+             "views/stock_buffer_profile_lead_time_view.xml",
              "views/stock_buffer_profile_view.xml",
              "views/product_adu_calculation_method_view.xml",
              "views/stock_warehouse_orderpoint_view.xml",
              "views/procurement_order_view.xml",
-             "views/mrp_production_view.xml"
+             "views/mrp_production_view.xml",
+             "data/ir_cron.xml"
              ],
     "license": "AGPL-3",
     'installable': True,
