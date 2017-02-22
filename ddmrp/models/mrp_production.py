649e30a945450c5bbb83efc9b1df9ba13667bcf5
Jordi Ballester <jordi.ballester@eficent.com>
2017-02-22 15:36:28 +0100

    Migration to v9

--- ddmrp/models/mrp_production.py
+++ ddmrp/models/mrp_production.py
@@ -4,7 +4,7 @@
 # © 2016 Aleph Objects, Inc. (https://www.alephobjects.com/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models, _
+from openerp import api, fields, models
 from .stock_warehouse_orderpoint import _PRIORITY_LEVEL
 
 
@@ -15,9 +15,13 @@ class MrpProduction(models.Model):
     def _search_procurements(self):
         return [('production_id', '=', self.id)]
 
+    @api.model
+    def _search_orderpoints(self):
+        return [('name', '=', self.move_prod_id.origin)]
+
     @api.multi
-    @api.depends("location_dest_id", "product_id")
-    def _compute_reordering_rule(self):
+    @api.depends('move_prod_id')
+    def _compute_orderpoint_id(self):
         for rec in self:
             domain = rec._search_procurements()
             procurements = rec.env['procurement.order'].search(domain)
@@ -25,6 +29,12 @@ class MrpProduction(models.Model):
                            procurements if procurement.orderpoint_id]
             if orderpoints:
                 rec.orderpoint_id = orderpoints[0]
+            else:
+                domain = rec._search_orderpoints()
+                orderpoints = rec.env['stock.warehouse.orderpoint'].search(
+                    domain)
+                if orderpoints:
+                    rec.orderpoint_id = orderpoints[0]
 
     @api.multi
     @api.depends("orderpoint_id")
@@ -33,7 +43,7 @@ class MrpProduction(models.Model):
             if rec.state not in ['done', 'cancel']:
                 rec.execution_priority_level = \
                     rec.orderpoint_id.execution_priority_level
-                rec.execution_priority = rec.orderpoint_id.execution_priority
+                rec.on_hand_percent = rec.orderpoint_id.on_hand_percent
 
     def _search_execution_priority(self, operator, value):
         """Search on the execution priority by evaluating on all
@@ -61,12 +71,11 @@ class MrpProduction(models.Model):
 
     orderpoint_id = fields.Many2one(
         comodel_name='stock.warehouse.orderpoint',
-        string="Reordering rule", compute='_compute_reordering_rule')
+        string="Reordering rule", compute='_compute_orderpoint_id')
     execution_priority_level = fields.Selection(
         string="Buffer On-Hand Alert Level",
         selection=_PRIORITY_LEVEL,
         compute="_compute_execution_priority",
         search="_search_execution_priority")
-    execution_priority = fields.Char(
-        string="Buffer On-Hand Alert",
-        compute='_compute_execution_priority')
+    on_hand_percent = fields.Float(string="On Hand/TOG (%)",
+                                   compute="_compute_execution_priority")
