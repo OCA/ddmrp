4286fccbce6bbd6e8c86cedf84728379c3372a31
Jordi Ballester <jordi.ballester@eficent.com>
2017-02-22 15:36:28 +0100

    Migration to v9

--- ddmrp/models/__init__.py
+++ ddmrp/models/__init__.py
@@ -3,7 +3,8 @@
 #   (http://www.eficent.com)
 # © 2016 Aleph Objects, Inc. (https://www.alephobjects.com/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
+from . import stock_buffer_profile_variability
+from . import stock_buffer_profile_lead_time
 from . import stock_buffer_profile
 from . import procurement_order
 from . import product_adu_calculation_method
