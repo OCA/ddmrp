2a56ec2129841bb34d14929a944483894d91a8a5
Jordi Ballester <jordi.ballester@eficent.com>
2017-06-12 15:19:34 +0200

    add module ddmrp_exclude_moves_adu_calc

---
+++ ddmrp_exclude_moves_adu_calc/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+        "name": "DDMRP Exclude Moves ADU Calc",
+    "summary": "Define additional rules to exclude certain moves from ADU "
+               "calculation",
+    "version": "9.0.1.0.0",
+    "author": "Eficent,"
+              "Odoo Community Association (OCA)",
+    "website": "https://www.odoo-community.org",
+    "category": "Warehouse Management",
+    "depends": ["ddmrp"],
+    "data": ["views/stock_move_view.xml"],
+    "license": "AGPL-3",
+    'installable': True,
+    'application': False,
+}
