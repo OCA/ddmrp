4672d2724cd96130645865fd541b02dce27a94a6
lreficent <lois.rilo@eficent.com>
2017-08-23 10:27:14 +0200

    [9.0][IMP] ddmrp_mrp_production_request: able to sort by exectution priority.

---
+++ ddmrp_mrp_production_request/models/stock_warehouse_orderpoint.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models
+
+
+class StockWarehouseOrderpoint(models.Model):
+    _inherit = 'stock.warehouse.orderpoint'
+
+    mrp_production_request_ids = fields.One2many(
+        string='Manufacturing Requests', comodel_name='mrp.production.request',
+        inverse_name='orderpoint_id')
+
+    @api.multi
+    def cron_actions(self):
+        res = super(StockWarehouseOrderpoint, self).cron_actions()
+        self.mrp_production_request_ids._compute_execution_priority()
+        return res
