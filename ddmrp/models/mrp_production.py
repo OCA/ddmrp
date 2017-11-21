8713eeae78236bcbce67c613d104d37179d85d26
lreficent <lois.rilo@eficent.com>
2017-11-21 12:47:47 +0100

    [9.0][IMP] ddmrp: enhance performace

--- ddmrp/models/mrp_production.py
+++ ddmrp/models/mrp_production.py
@@ -50,15 +50,17 @@ class MrpProduction(models.Model):
 
     @api.multi
     @api.depends('orderpoint_id', 'procurement_ids', 'move_prod_id')
-    def _compute_execution_priority(self):
-        for rec in self.filtered(lambda r: r.orderpoint_id):
-            if rec.state in ['done', 'cancel']:
-                rec.execution_priority_level = None
-                rec.on_hand_percent = None
-            else:
-                rec.execution_priority_level = \
-                    rec.orderpoint_id.execution_priority_level
-                rec.on_hand_percent = rec.orderpoint_id.on_hand_percent
+    def _calc_execution_priority(self):
+        prods = self.filtered(
+            lambda r: r.orderpoint_id and r.state not in ['done', 'cancel'])
+        for rec in prods:
+            rec.execution_priority_level = \
+                rec.orderpoint_id.execution_priority_level
+            rec.on_hand_percent = rec.orderpoint_id.on_hand_percent
+        (self - prods).write({
+            'execution_priority_level': None,
+            'on_hand_percent': None,
+        })
 
     def _search_execution_priority(self, operator, value):
         """Search on the execution priority by evaluating on all
@@ -87,16 +89,15 @@ class MrpProduction(models.Model):
     orderpoint_id = fields.Many2one(
         comodel_name='stock.warehouse.orderpoint', store=True, index=True,
         string="Reordering rule", compute='_compute_orderpoint_id')
-    procurement_ids = fields.One2many(comodel_name='procurement.order',
-                                      inverse_name='production_id',
-                                      string='Procurements')
-
+    procurement_ids = fields.One2many(
+        comodel_name='procurement.order',
+        inverse_name='production_id',
+        string='Procurements',
+    )
     execution_priority_level = fields.Selection(
         string="Buffer On-Hand Alert Level",
-        selection=_PRIORITY_LEVEL,
-        compute="_compute_execution_priority",
-        search="_search_execution_priority",
-        store=True)
+        selection=_PRIORITY_LEVEL, readonly=True,
+    )
     on_hand_percent = fields.Float(
-        string="On Hand/TOG (%)", compute="_compute_execution_priority",
-        store=True)
+        string="On Hand/TOG (%)", compute="_calc_execution_priority",
+        store=True)  # TODO: remove compute and store.
