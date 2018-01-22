d81ea6c53fb7b359f2d89fbe3583e2b4e6271758
Lois Rilo <lois.rilo@eficent.com>
2018-01-22 17:18:11 +0100

    [10.0][MIG] ddmrp_exclude_moves_adu_cal

--- ddmrp_exclude_moves_adu_calc/models/stock_warehouse_orderpoint.py
+++ ddmrp_exclude_moves_adu_calc/models/stock_warehouse_orderpoint.py
@@ -1,30 +1,25 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models
+from odoo import api, models
 
 
 class StockWarehouseOrderpoint(models.Model):
     _inherit = "stock.warehouse.orderpoint"
 
     @api.model
-    def _exclude_past_moves_domain(self, locations):
+    def _exclude_past_moves_domain(self):
         return [('exclude_from_adu', '=', True), ('state', '=', 'done')]
 
     @api.model
     def _past_moves_domain(self, date_from, locations):
-
-        exclude_locs = self.env['stock.location'].search([(
-            'exclude_from_adu', '=', True)])
-        new_locs = self.env['stock.location']
-        for loc in locations:
-            if loc not in exclude_locs:
-                new_locs += loc
+        new_locs = locations.filtered(lambda l: not l.exclude_from_adu)
         res = super(StockWarehouseOrderpoint,
                     self)._past_moves_domain(date_from, new_locs)
         exclude_moves = self.env['stock.move'].search(
-            self._exclude_past_moves_domain(new_locs))
-        res.append(('id', 'not in', exclude_moves.ids))
+            self._exclude_past_moves_domain())
+        if exclude_moves:
+            res.append(('id', 'not in', exclude_moves.ids))
         return res
