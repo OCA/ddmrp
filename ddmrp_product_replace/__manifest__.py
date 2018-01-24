bd0839b7f691cf20a5607028405e6897c9b0f83e
Lois Rilo <lois.rilo@eficent.com>
2018-01-24 14:25:40 +0100

    [10.0][MIG] ddmrp_product_replace

--- ddmrp_product_replace/__manifest__.py
+++ ddmrp_product_replace/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "DDMRP Product Replace",
     "summary": "Provides a assisting tool for product replacement.",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Eficent",
     "website": "http://www.eficent.com",
     "category": "Warehouse Management",
