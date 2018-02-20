84e4e33d3a388b144422f741c69d426d02cfec33
Lois Rilo <lois.rilo@eficent.com>
2018-02-20 18:00:05 +0100

    [9.0][IMP] ddmrp_adjustment: consider lead time on demand alocation for DAF

--- ddmrp_adjustment/models/ddmrp_adjustment_demand.py
+++ ddmrp_adjustment/models/ddmrp_adjustment_demand.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -10,10 +10,23 @@ class DdmrpAdjustmentDemand(models.Model):
     _name = "ddmrp.adjustment.demand"
 
     buffer_id = fields.Many2one(
-        comodel_name="stock.warehouse.orderpoint", string="Apply to")
+        comodel_name="stock.warehouse.orderpoint", string="Apply to",
+    )
+    product_id = fields.Many2one(related="buffer_id.product_id")
     buffer_origin_id = fields.Many2one(
-        comodel_name="stock.warehouse.orderpoint", string="Originated from")
+        comodel_name="stock.warehouse.orderpoint", string="Originated from",
+    )
+    product_origin_id = fields.Many2one(related="buffer_origin_id.product_id")
     extra_demand = fields.Float(string="Extra Demand")
     product_uom_id = fields.Many2one(
         comodel_name="product.uom", string="Unit of Measure",
-        related="buffer_id.product_uom")
+        related="buffer_id.product_uom",
+    )
+    date_start = fields.Date(
+        string='Start date',
+    )
+    date_end = fields.Date(
+        string='End date',
+    )
+
+    # TODO: everything READONLY?
