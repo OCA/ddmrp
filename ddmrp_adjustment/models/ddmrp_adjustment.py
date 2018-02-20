d260742eaa392b60c63878bed2e9ff0a678f079e
Lois Rilo <lois.rilo@eficent.com>
2018-02-20 18:00:05 +0100

    [9.0][IMP] ddmrp_adjustment: consider lead time on demand alocation for DAF

--- ddmrp_adjustment/models/ddmrp_adjustment.py
+++ ddmrp_adjustment/models/ddmrp_adjustment.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from openerp import api, fields, models
+
+DAF_string = u"DAF"
+LTAF_string = u"LTAF"  # TODO: finish this
 
 
 class DdmrpAdjustment(models.Model):
@@ -11,18 +14,27 @@ class DdmrpAdjustment(models.Model):
 
     buffer_id = fields.Many2one(
         comodel_name="stock.warehouse.orderpoint", string="Buffer",
-        required=True)
+        required=True,
+    )
     product_id = fields.Many2one(
         comodel_name="product.product", related="buffer_id.product_id",
-        readonly=True)
+        readonly=True,
+    )
     location_id = fields.Many2one(
         comodel_name="stock.location", related="buffer_id.location_id",
-        readonly=True)
+        readonly=True,
+    )
     date_range_id = fields.Many2one(
-        comodel_name="date.range", string="Date Range", required=True)
+        comodel_name="date.range", string="Date Range", required=True,
+    )
     daf = fields.Float(
-        string="DAF", help="Demand Adjustment Factor", default=0.0)
+        string=DAF_string, help="Demand Adjustment Factor", default=0.0,
+    )
+    ltaf = fields.Float(
+        string=LTAF_string, help="Lead Time Ajustment Factor",
+    )
     company_id = fields.Many2one(
         comodel_name='res.company', string='Company', required=True,
         default=lambda self: self.env['res.company']._company_default_get(
-            'ddmrp.adjustment'))
+            'ddmrp.adjustment'),
+    )
