ac9ed0cd809a5a81d6d4243e9bf946e8525867d1
hveficent <hector.villarreal@eficent.com>
2018-02-21 12:13:58 +0100

    [MIG] ddmrp_adjustment: Migration to 10.0

--- ddmrp_history/models/ddmrp_history.py
+++ ddmrp_history/models/ddmrp_history.py
@@ -3,8 +3,8 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
-from openerp.addons import decimal_precision as dp
+from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
 
 UNIT = dp.get_precision('Product Unit of Measure')
 
