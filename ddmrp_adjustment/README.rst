b96dc880986ba99df5944ed7bd76ef664294a301
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-03-08 13:11:52 +0100

    [MIG] ddmrp_adjustment: Migration to 11.0

--- ddmrp_adjustment/README.rst
+++ ddmrp_adjustment/README.rst
@@ -61,6 +61,7 @@ Contributors
 
 * Lois Rilo <lois.rilo@eficent.com>
 * Jordi Ballester <jordi.ballester@eficent.com>
+* Akim Juillerat <akim.juillerat@camptocamp.com>
 
 Maintainer
 ==========
---
+++ ddmrp_adjustment/tests/test_adu_dlt_adjustment.py
@@ -0,0 +1,45 @@
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from .test_common import TestDDMRPAdjustmentCommon
+
+
+class TestAduAdjustment(TestDDMRPAdjustmentCommon):
+
+    def setUp(self):
+        super().setUp()
+        self.adu_before = self.orderpoint.adu
+        self.dlt_before = self.orderpoint.dlt
+
+    def test_adu_adjustment(self):
+        wiz = self._create_adjustment_wizard(1)
+        wiz.apply_daf = True
+        wiz._onchange_sheet()
+
+        values = {
+            getattr(self, 'month_%i_%i' % (
+                self.now.year, self.now.month)): 1.5,
+        }
+        for line in wiz.line_ids:
+            line.value = values.get(line.date_range_id)
+        wiz.button_validate()
+
+        self.env['stock.warehouse.orderpoint'].cron_ddmrp_adu()
+
+        self.assertEqual(self.orderpoint.adu, self.adu_before * 1.5)
+
+    def test_dlt_adjustment(self):
+        wiz = self._create_adjustment_wizard(1)
+        wiz.apply_ltaf = True
+        wiz._onchange_sheet()
+
+        values = {
+            getattr(self, 'month_%i_%i' % (
+                self.now.year, self.now.month)): 2,
+        }
+        for line in wiz.line_ids:
+            line.value = values.get(line.date_range_id)
+        wiz.button_validate()
+
+        self.env['stock.warehouse.orderpoint'].cron_ddmrp_adu()
+
+        self.assertEqual(self.orderpoint.dlt, self.dlt_before * 2)
