1347c117676cf94a12e055fd90d143073f40a781
Antonio Yamuta <agyamuta@users.noreply.github.com>
2018-03-30 00:22:09 -0700

    [ADD] ddmrp_production_equivalent module

---
+++ ddmrp_production_equivalent/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "MRP BoM Equivalences when Production Order is Created",
+    "summary": "Use equivalences of a part when appropriate",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "MRP",
+    "website": "http://www.opensourceintegrators.com",
+    "depends": [
+        "ddmrp",
+        "mrp",
+        "mrp_bom_equivalent",
+        "product_priority",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
---
+++ ddmrp_production_equivalent/models/mrp_production.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    def _get_product_search_criteria(self, bom_line):
+        return [('categ_id', 'child_of', bom_line.product_id.categ_id.id)]
+
+    def _get_product_equivalent(self, bom_line, requested_qty):
+        # get all the other products in the same product category
+        p_obj = self.env['product.product']
+        products = p_obj.search(self._get_product_search_criteria(bom_line),
+                                order='priority asc, id asc')
+        # exclude the non-equivalent parts listed in the BOM line
+        products -= bom_line.nonequivalent_product_ids
+        product = False
+        for product in products:
+            if product.orderpoint_ids and \
+               product.orderpoint_ids[0].planning_priority_level == '3_green':
+                    if product.qty_available > requested_qty:
+                        break
+        return product
+
+    def _generate_raw_move(self, bom_line, line_data):
+        sm = super(MrpProduction, self)._generate_raw_move(bom_line, line_data)
+        if not bom_line.use_equivalences:
+            return sm
+        if (bom_line.orderpoint_id.planning_priority_level == '3_green' and
+                bom_line.product_id.qty_available > line_data['qty']):
+            return sm
+
+        product_equivalent = self._get_product_equivalent(bom_line,
+                                                          line_data['qty'])
+        if product_equivalent:
+            sm.write({
+                'price_unit': product_equivalent.standard_price,
+                'product_id': product_equivalent.id,
+            })
+        return sm
