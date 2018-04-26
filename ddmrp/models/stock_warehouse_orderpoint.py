37ffffee2cf7f0b29236ceac9b74169f236117b9
Lois Rilo <lois.rilo@eficent.com>
2018-04-26 11:15:49 +0200

    [11.0][FIX] ADU *must* only be computed by the cron job (we do *not* want real-time ADU).

--- ddmrp/models/stock_warehouse_orderpoint.py
+++ ddmrp/models/stock_warehouse_orderpoint.py
@@ -342,19 +342,6 @@ class StockWarehouseOrderpoint(models.Model):
 
     _order = 'planning_priority_level asc, net_flow_position asc'
 
-    @api.model
-    def create(self, vals):
-        record = super(StockWarehouseOrderpoint, self).create(vals)
-        record._calc_adu()
-        return record
-
-    @api.multi
-    def write(self, vals):
-        super(StockWarehouseOrderpoint, self).write(vals)
-        if not self.env.context.get('__no_adu_calc'):
-            self._calc_adu()
-        return True
-
     @api.multi
     @api.onchange("red_zone_qty")
     def onchange_red_zone_qty(self):
@@ -505,7 +492,7 @@ class StockWarehouseOrderpoint(models.Model):
 
     @api.multi
     def _calc_adu(self):
-        for orderpoint in self.with_context(__no_adu_calc=True):
+        for orderpoint in self:
             if orderpoint.adu_calculation_method.method == 'fixed':
                 orderpoint.adu = orderpoint.adu_fixed
             elif orderpoint.adu_calculation_method.method == 'past':
