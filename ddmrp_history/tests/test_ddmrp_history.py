6c334ccbcf8d9f8be211c0e67bdd6e6dd0a76303
Lois Rilo <lois.rilo@eficent.com>
2018-08-01 10:39:31 +0200

    [11.0][IMP] ddmrp_history to OCA: * update ddmrp_history README and manifest to follow OCA guideliens and add new icon. * add context to be able to avoid history creation. * improve tests. * solve todo comments.

--- ddmrp_history/tests/test_ddmrp_history.py
+++ ddmrp_history/tests/test_ddmrp_history.py
@@ -1,17 +1,17 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from odoo.tests import TransactionCase
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo import fields
 
 
 class TestDDMRPHistory(TransactionCase):
 
     def setUp(self):
         super().setUp()
-        self.orderpoint = self.env.ref('ddmrp.stock_warehouse_orderpoint_901p')
+        self.orderpoint = self.env.ref('ddmrp.stock_warehouse_orderpoint_fp01')
 
     def test_history(self):
         self.orderpoint.write({
@@ -24,11 +24,15 @@ class TestDDMRPHistory(TransactionCase):
             ('orderpoint_id', '=', self.orderpoint.id)],
             order='date desc', limit=1
         )
-        self.assertEqual(history_today.date, datetime.today().strftime(
-            DEFAULT_SERVER_DATETIME_FORMAT))
+        self.assertAlmostEqual(fields.Datetime.from_string(history_today.date),
+                               datetime.today(), delta=timedelta(seconds=1))
         self.assertEqual(history_today.top_of_red,
                          self.orderpoint.top_of_red)
         self.assertEqual(history_today.top_of_yellow,
                          self.orderpoint.top_of_yellow)
         self.assertEqual(history_today.top_of_green,
                          self.orderpoint.top_of_green)
+        # Check that chart computation do not raise an error:
+        self.orderpoint.cron_actions()
+        self.assertTrue(self.orderpoint.planning_history_chart)
+        self.assertTrue(self.orderpoint.execution_history_chart)
