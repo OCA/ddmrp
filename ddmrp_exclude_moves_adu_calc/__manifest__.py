2eddce136787d088fbc11386d061c6196c745464
Lois Rilo <lois.rilo@eficent.com>
2018-09-17 16:32:28 +0200

    [11.0][MIG] ddmrp_exclude_moves_adu_calc

--- ddmrp_exclude_moves_adu_calc/__manifest__.py
+++ ddmrp_exclude_moves_adu_calc/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -7,10 +6,11 @@
     "name": "DDMRP Exclude Moves ADU Calc",
     "summary": "Define additional rules to exclude certain moves from ADU "
                "calculation",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent,"
               "Odoo Community Association (OCA)",
-    "website": "https://www.odoo-community.org",
+    "maintainers": ['jbeficent', 'lreficent'],
+    "website": "https://github.com/OCA/ddmrp",
     "category": "Warehouse Management",
     "depends": ["ddmrp"],
     "data": [
