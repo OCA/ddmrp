e55e466cc84d8212becb18a97dc14942fe7610b6
Lois Rilo <lois.rilo@eficent.com>
2018-09-17 16:32:28 +0200

    [11.0][MIG] ddmrp_exclude_moves_adu_calc

--- ddmrp_exclude_moves_adu_calc/models/stock_move.py
+++ ddmrp_exclude_moves_adu_calc/models/stock_move.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class StockMove(models.Model):
@@ -14,3 +14,11 @@ class StockMove(models.Model):
         help="If this flag is set this stock move will be excluded from ADU "
              "calculation",
     )
+
+    @api.multi
+    def _toggle_exclude_from_adu(self):
+        if not self.env.user.has_group("stock.group_stock_manager"):
+            raise UserError(_(
+                "Only inventory managers are allowed perform this action."))
+        for rec in self:
+            rec.exclude_from_adu = not rec.exclude_from_adu
