70c7a3744ec11f82b8edd99e66056be71e83f363
Lois Rilo <lois.rilo@eficent.com>
2018-10-31 10:33:06 +0100

    [11.0][IMP] ddmrp_product_replace: the products considered as demand are visible after a executing a product replacement.

--- ddmrp_product_replace/models/stock_warehouse_orderpoint.py
+++ ddmrp_product_replace/models/stock_warehouse_orderpoint.py
@@ -3,7 +3,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class StockWarehouseOrderpoint(models.Model):
@@ -12,8 +13,16 @@ class StockWarehouseOrderpoint(models.Model):
     demand_product_ids = fields.Many2many(
         comodel_name="product.product", string="Considered As Demand",
         help="This field is used for a correct product replacement within a "
-             "DDMRP buffer.", readonly=True,
+             "DDMRP buffer.",
     )
+
+    @api.constrains('demand_product_ids')
+    def _check_demand_product_ids(self):
+        for rec in self:
+            if (rec.demand_product_ids and
+                    rec.product_id not in rec.demand_product_ids):
+                raise ValidationError(
+                    _("Buffered product must be considered as demand."))
 
     @api.multi
     def _past_moves_domain(self, date_from, locations):
