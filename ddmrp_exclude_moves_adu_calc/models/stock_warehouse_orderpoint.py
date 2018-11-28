6d6d7f96add424b0e29e01e9978b025fe20050e4
Lois Rilo <lois.rilo@eficent.com>
2018-11-28 09:43:38 +0100

    [11.0][FIX] ddmrp: restrict adu domains

--- ddmrp_exclude_moves_adu_calc/models/stock_warehouse_orderpoint.py
+++ ddmrp_exclude_moves_adu_calc/models/stock_warehouse_orderpoint.py
@@ -13,10 +13,10 @@ class StockWarehouseOrderpoint(models.Model):
         return [('exclude_from_adu', '=', True)]
 
     @api.model
-    def _past_moves_domain(self, date_from, locations):
+    def _past_moves_domain(self, date_from, date_to, locations):
         new_locs = locations.filtered(lambda l: not l.exclude_from_adu)
         res = super(StockWarehouseOrderpoint,
-                    self)._past_moves_domain(date_from, new_locs)
+                    self)._past_moves_domain(date_from, date_to, new_locs)
         exclude_moves = self.env['stock.move'].search(
             self._exclude_past_moves_domain())
         if exclude_moves:
