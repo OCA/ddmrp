ebacae2220c59dd9eb3b489ae11a2de05a71d2f2
Lois Rilo <lois.rilo@eficent.com>
2019-06-03 13:44:12 +0200

    [11.0][FIX] ddmrp: consider dependent requirements when computing the qualified demand.

--- ddmrp/wizards/multi_level_mrp.py
+++ ddmrp/wizards/multi_level_mrp.py
@@ -8,14 +8,14 @@ class MultiLevelMrp(models.TransientModel):
     _inherit = 'mrp.multi.level'
 
     @api.model
-    def _exclude_from_mrp(self, mrp_area, product):
+    def _exclude_from_mrp(self, product, mrp_area):
         """ Exclude from MRP scheduler products that are buffered. """
-        res = super(MultiLevelMrp, self)._exclude_from_mrp(mrp_area, product)
+        res = super(MultiLevelMrp, self)._exclude_from_mrp(product, mrp_area)
         ddmrp = self.env['stock.warehouse.orderpoint'].search([
             ('location_id', '=', mrp_area.location_id.id),  # child of?
             ('product_id', '=', product.id),
             ('buffer_profile_id', '!=', False),
         ])
-        if ddmrp:
+        if ddmrp and not self.env.context.get("mrp_explosion"):
             return True
         return res
