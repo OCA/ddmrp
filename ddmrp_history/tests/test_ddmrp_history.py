1c181b106472dfb98b1485094d8f51c60719c4af
Lois Rilo <lois.rilo@forgeflow.com>
2020-06-16 15:59:58 +0200

    [IMP] ddmrp_history: black, isort, prettier

--- ddmrp_history/tests/test_ddmrp_history.py
+++ ddmrp_history/tests/test_ddmrp_history.py
@@ -4,35 +4,31 @@
 
 from datetime import datetime, timedelta
 
-from odoo.tests import TransactionCase
 from odoo import fields
+from odoo.tests import TransactionCase
 
 
 class TestDDMRPHistory(TransactionCase):
-
     def setUp(self):
         super().setUp()
-        self.buffer = self.env.ref('ddmrp.stock_buffer_fp01')
+        self.buffer = self.env.ref("ddmrp.stock_buffer_fp01")
 
     def test_history(self):
-        self.buffer.write({
-            'adu_fixed': 10,
-            'order_cycle': 5,
-            'order_spike_horizon': 10
-        })
-        self.buffer.cron_actions()
-        history_today = self.env['ddmrp.history'].search([
-            ('buffer_id', '=', self.buffer.id)],
-            order='date desc', limit=1
+        self.buffer.write(
+            {"adu_fixed": 10, "order_cycle": 5, "order_spike_horizon": 10}
         )
-        self.assertAlmostEqual(fields.Datetime.from_string(history_today.date),
-                               datetime.today(), delta=timedelta(seconds=1))
-        self.assertEqual(history_today.top_of_red,
-                         self.buffer.top_of_red)
-        self.assertEqual(history_today.top_of_yellow,
-                         self.buffer.top_of_yellow)
-        self.assertEqual(history_today.top_of_green,
-                         self.buffer.top_of_green)
+        self.buffer.cron_actions()
+        history_today = self.env["ddmrp.history"].search(
+            [("buffer_id", "=", self.buffer.id)], order="date desc", limit=1
+        )
+        self.assertAlmostEqual(
+            fields.Datetime.from_string(history_today.date),
+            datetime.today(),
+            delta=timedelta(seconds=1),
+        )
+        self.assertEqual(history_today.top_of_red, self.buffer.top_of_red)
+        self.assertEqual(history_today.top_of_yellow, self.buffer.top_of_yellow)
+        self.assertEqual(history_today.top_of_green, self.buffer.top_of_green)
         # Check that chart computation do not raise an error:
         self.buffer.cron_actions()
         self.assertTrue(self.buffer.planning_history_chart)
