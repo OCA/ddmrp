acb65f1bd84dd3342f1a30cc96e4c0cd4278c55e
Lois Rilo <lois.rilo@forgeflow.com>
2020-06-16 15:58:06 +0200

    [13.0][MIG] ddmrp_history

--- ddmrp_history/__manifest__.py
+++ ddmrp_history/__manifest__.py
@@ -1,28 +1,27 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# Copyright 2017-20 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "DDMRP History",
     "summary": "Allow to store historical data of DDMRP buffers.",
-    "version": "11.0.1.0.1",
+    "version": "13.0.1.0.0",
     "development_status": "Beta",
-    "author": "Eficent, Odoo Community Association (OCA)",
-    "maintainers": ['jbeficent', 'lreficent'],
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "maintainers": ["JordiBForgeFlow", "LoisRForgeFlow"],
     "website": "https://github.com/OCA/ddmrp",
     "category": "Warehouse Management",
     "depends": ["ddmrp"],
     "data": [
         "security/ir.model.access.csv",
         "views/ddmrp_history_view.xml",
-        "views/stock_warehouse_orderpoint_view.xml",
+        "views/stock_buffer_view.xml",
     ],
     "demo": [
-        'demo/ddmrp.history.csv',
+        "demo/ddmrp.history.csv",
     ],
     "external_dependencies": {
-        "python": ['pandas'],
+        "python": ["pandas"],
     },
-    "license": "AGPL-3",
-    'installable': True,
+    "license": "LGPL-3",
+    "installable": True,
 }
