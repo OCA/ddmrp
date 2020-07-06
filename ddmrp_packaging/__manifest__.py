60eb19844b79c17a294912a8fb97b4eae4b728f4
Lois Rilo <lois.rilo@forgeflow.com>
2020-07-06 12:47:14 +0200

    [13.0][ADD] ddmrp_packaging

---
+++ ddmrp_packaging/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "DDMRP Packaging",
+    "summary": "DDMRP integration with packaging",
+    "version": "13.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://www.forgeflow.com",
+    "category": "Warehouse Management",
+    "depends": ["ddmrp"],
+    "data": ["views/stock_buffer_view.xml"],
+    "license": "LGPL-3",
+    "installable": True,
+    "auto_install": False,
+}
