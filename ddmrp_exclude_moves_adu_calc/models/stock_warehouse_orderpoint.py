b262b0b76d9382fb97f5a08701ef53b55c819e24
Lois Rilo <lois.rilo@forgeflow.com>
2021-03-15 09:24:41 +0100

    [IMP] ddmrp_exclude_moves_adu_calc: black, isort, prettier

--- ddmrp_exclude_moves_adu_calc/models/stock_warehouse_orderpoint.py
+++ ddmrp_exclude_moves_adu_calc/models/stock_warehouse_orderpoint.py
@@ -10,15 +10,15 @@ class StockWarehouseOrderpoint(models.Model):
 
     @api.model
     def _exclude_past_moves_domain(self):
-        return [('exclude_from_adu', '=', True)]
+        return [("exclude_from_adu", "=", True)]
 
     @api.model
     def _past_moves_domain(self, date_from, date_to, locations):
         new_locs = locations.filtered(lambda l: not l.exclude_from_adu)
-        res = super(StockWarehouseOrderpoint,
-                    self)._past_moves_domain(date_from, date_to, new_locs)
-        exclude_moves = self.env['stock.move'].search(
-            self._exclude_past_moves_domain())
+        res = super(StockWarehouseOrderpoint, self)._past_moves_domain(
+            date_from, date_to, new_locs
+        )
+        exclude_moves = self.env["stock.move"].search(self._exclude_past_moves_domain())
         if exclude_moves:
-            res.append(('id', 'not in', exclude_moves.ids))
+            res.append(("id", "not in", exclude_moves.ids))
         return res
--- ddmrp_exclude_moves_adu_calc/tests/test_ddmrp.py
+++ ddmrp_exclude_moves_adu_calc/tests/test_ddmrp.py
@@ -3,126 +3,151 @@
 # Copyright 2016 Aleph Objects, Inc. (https://www.alephobjects.com/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-import odoo.tests.common as common
 from datetime import datetime, timedelta
+
+import odoo.tests.common as common
 
 
 class TestDdmrp(common.TransactionCase):
-
     def setUp(self):
         super(TestDdmrp, self).setUp()
 
         # Models
-        self.productModel = self.env['product.product']
-        self.orderpointModel = self.env['stock.warehouse.orderpoint']
-        self.pickingModel = self.env['stock.picking']
-        self.quantModel = self.env['stock.quant']
-        self.aducalcmethodModel = self.env['product.adu.calculation.method']
-        self.locationModel = self.env['stock.location']
-        self.make_procurement_orderpoint_model =\
-            self.env['make.procurement.orderpoint']
-        self.user_model = self.env['res.users']
+        self.productModel = self.env["product.product"]
+        self.orderpointModel = self.env["stock.warehouse.orderpoint"]
+        self.pickingModel = self.env["stock.picking"]
+        self.quantModel = self.env["stock.quant"]
+        self.aducalcmethodModel = self.env["product.adu.calculation.method"]
+        self.locationModel = self.env["stock.location"]
+        self.make_procurement_orderpoint_model = self.env["make.procurement.orderpoint"]
+        self.user_model = self.env["res.users"]
 
         # Refs
-        self.main_company = self.env.ref('base.main_company')
-        self.warehouse = self.env.ref('stock.warehouse0')
-        self.stock_location = self.env.ref('stock.stock_location_stock')
-        self.location_shelf1 = self.env.ref('stock.stock_location_components')
-        self.supplier_location = self.env.ref('stock.stock_location_suppliers')
-        self.customer_location = self.env.ref('stock.stock_location_customers')
-        self.uom_unit = self.env.ref('product.product_uom_unit')
+        self.main_company = self.env.ref("base.main_company")
+        self.warehouse = self.env.ref("stock.warehouse0")
+        self.stock_location = self.env.ref("stock.stock_location_stock")
+        self.location_shelf1 = self.env.ref("stock.stock_location_components")
+        self.supplier_location = self.env.ref("stock.stock_location_suppliers")
+        self.customer_location = self.env.ref("stock.stock_location_customers")
+        self.uom_unit = self.env.ref("product.product_uom_unit")
         self.buffer_profile_pur = self.env.ref(
-            'ddmrp.stock_buffer_profile_replenish_purchased_short_low')
-        self.group_stock_manager = self.env.ref('stock.group_stock_manager')
+            "ddmrp.stock_buffer_profile_replenish_purchased_short_low"
+        )
+        self.group_stock_manager = self.env.ref("stock.group_stock_manager")
 
         # Create users
-        self.user = self._create_user('user_1',
-                                      [self.group_stock_manager])
+        self.user = self._create_user("user_1", [self.group_stock_manager])
 
         self.productA = self.productModel.create(
-            {'name': 'product A',
-             'standard_price': 1,
-             'type': 'product',
-             'uom_id': self.uom_unit.id,
-             'default_code': 'A',
-             })
+            {
+                "name": "product A",
+                "standard_price": 1,
+                "type": "product",
+                "uom_id": self.uom_unit.id,
+                "default_code": "A",
+            }
+        )
 
-        self.inventory_loc = self.locationModel.create({
-            'usage': 'inventory',
-            'name': 'Inventory',
-            'company_id': self.main_company.id
-        })
+        self.inventory_loc = self.locationModel.create(
+            {
+                "usage": "inventory",
+                "name": "Inventory",
+                "company_id": self.main_company.id,
+            }
+        )
 
-        self.binA = self.locationModel.create({
-            'usage': 'internal',
-            'name': 'Bin A',
-            'location_id': self.location_shelf1.id,
-            'company_id': self.main_company.id
-        })
+        self.binA = self.locationModel.create(
+            {
+                "usage": "internal",
+                "name": "Bin A",
+                "location_id": self.location_shelf1.id,
+                "company_id": self.main_company.id,
+            }
+        )
 
-        self.binB = self.locationModel.create({
-            'usage': 'internal',
-            'name': 'Bin B',
-            'location_id': self.location_shelf1.id,
-            'company_id': self.main_company.id
-        })
+        self.binB = self.locationModel.create(
+            {
+                "usage": "internal",
+                "name": "Bin B",
+                "location_id": self.location_shelf1.id,
+                "company_id": self.main_company.id,
+            }
+        )
 
         self.locationModel._parent_store_compute()
 
         self.quant = self.quantModel.create(
-            {'location_id': self.binA.id,
-             'company_id': self.main_company.id,
-             'product_id': self.productA.id,
-             'quantity': 200.0})
+            {
+                "location_id": self.binA.id,
+                "company_id": self.main_company.id,
+                "product_id": self.productA.id,
+                "quantity": 200.0,
+            }
+        )
 
     def _create_user(self, login, groups):
         """ Create a user."""
         group_ids = [group.id for group in groups]
-        user = \
-            self.user_model.with_context({'no_reset_password': True}).create({
-                'name': 'Test User',
-                'login': login,
-                'password': 'demo',
-                'email': 'test@yourcompany.com',
-                'groups_id': [(6, 0, group_ids)]
-            })
+        user = self.user_model.with_context({"no_reset_password": True}).create(
+            {
+                "name": "Test User",
+                "login": login,
+                "password": "demo",
+                "email": "test@yourcompany.com",
+                "groups_id": [(6, 0, group_ids)],
+            }
+        )
         return user
 
     def create_pickingoutA(self, date_move, qty):
-        return self.pickingModel.sudo(self.user).create({
-            'picking_type_id': self.ref('stock.picking_type_out'),
-            'location_id': self.binA.id,
-            'location_dest_id': self.customer_location.id,
-            'move_lines': [
-                (0, 0, {
-                    'name': 'Test move',
-                    'product_id': self.productA.id,
-                    'date_expected': date_move,
-                    'date': date_move,
-                    'product_uom': self.productA.uom_id.id,
-                    'product_uom_qty': qty,
-                    'location_id': self.binA.id,
-                    'location_dest_id': self.customer_location.id,
-                })]
-        })
+        return self.pickingModel.sudo(self.user).create(
+            {
+                "picking_type_id": self.ref("stock.picking_type_out"),
+                "location_id": self.binA.id,
+                "location_dest_id": self.customer_location.id,
+                "move_lines": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "Test move",
+                            "product_id": self.productA.id,
+                            "date_expected": date_move,
+                            "date": date_move,
+                            "product_uom": self.productA.uom_id.id,
+                            "product_uom_qty": qty,
+                            "location_id": self.binA.id,
+                            "location_dest_id": self.customer_location.id,
+                        },
+                    )
+                ],
+            }
+        )
 
     def create_pickingoutB(self, date_move, qty):
-        return self.pickingModel.sudo(self.user).create({
-            'picking_type_id': self.ref('stock.picking_type_out'),
-            'location_id': self.binA.id,
-            'location_dest_id': self.inventory_loc.id,
-            'move_lines': [
-                (0, 0, {
-                    'name': 'Test move',
-                    'product_id': self.productA.id,
-                    'date_expected': date_move,
-                    'date': date_move,
-                    'product_uom': self.productA.uom_id.id,
-                    'product_uom_qty': qty,
-                    'location_id': self.binA.id,
-                    'location_dest_id': self.customer_location.id,
-                })]
-        })
+        return self.pickingModel.sudo(self.user).create(
+            {
+                "picking_type_id": self.ref("stock.picking_type_out"),
+                "location_id": self.binA.id,
+                "location_dest_id": self.inventory_loc.id,
+                "move_lines": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "Test move",
+                            "product_id": self.productA.id,
+                            "date_expected": date_move,
+                            "date": date_move,
+                            "product_uom": self.productA.uom_id.id,
+                            "product_uom_qty": qty,
+                            "location_id": self.binA.id,
+                            "location_dest_id": self.customer_location.id,
+                        },
+                    )
+                ],
+            }
+        )
 
     def _do_picking(self, picking, date):
         picking.action_confirm()
@@ -134,19 +159,21 @@ class TestDdmrp(common.TransactionCase):
 
     def test_01_exclude_move_from_adu(self):
 
-        method = self.env.ref('ddmrp.adu_calculation_method_past_120')
-        orderpointA = self.orderpointModel.create({
-            'buffer_profile_id': self.buffer_profile_pur.id,
-            'product_id': self.productA.id,
-            'location_id': self.stock_location.id,
-            'warehouse_id': self.warehouse.id,
-            'product_min_qty': 0.0,
-            'product_max_qty': 0.0,
-            'qty_multiple': 0.0,
-            'dlt': 10,
-            'adu_calculation_method': method.id,
-            'adu_fixed': 4
-        })
+        method = self.env.ref("ddmrp.adu_calculation_method_past_120")
+        orderpointA = self.orderpointModel.create(
+            {
+                "buffer_profile_id": self.buffer_profile_pur.id,
+                "product_id": self.productA.id,
+                "location_id": self.stock_location.id,
+                "warehouse_id": self.warehouse.id,
+                "product_min_qty": 0.0,
+                "product_max_qty": 0.0,
+                "qty_multiple": 0.0,
+                "dlt": 10,
+                "adu_calculation_method": method.id,
+                "adu_fixed": 4,
+            }
+        )
         self.orderpointModel.cron_ddmrp_adu()
 
         self.assertEqual(orderpointA.adu, 0)
