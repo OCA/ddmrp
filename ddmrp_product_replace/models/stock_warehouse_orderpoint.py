5e9c2d0408a848e1b9d7d165e7bc0e8683b762ee
Lois Rilo <lois.rilo@forgeflow.com>
2021-03-15 10:46:20 +0100

    [IMP] ddmrp_product_replace: black, isort, prettier

--- ddmrp_product_replace/models/stock_warehouse_orderpoint.py
+++ ddmrp_product_replace/models/stock_warehouse_orderpoint.py
@@ -3,33 +3,37 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class StockWarehouseOrderpoint(models.Model):
-    _inherit = 'stock.warehouse.orderpoint'
+    _inherit = "stock.warehouse.orderpoint"
 
     demand_product_ids = fields.Many2many(
-        comodel_name="product.product", string="Considered As Demand",
+        comodel_name="product.product",
+        string="Considered As Demand",
         help="This field is used for a correct product replacement within a "
-             "DDMRP buffer.",
+        "DDMRP buffer.",
     )
 
-    @api.constrains('demand_product_ids')
+    @api.constrains("demand_product_ids")
     def _check_demand_product_ids(self):
         for rec in self:
-            if (rec.demand_product_ids and
-                    rec.product_id not in rec.demand_product_ids):
+            if rec.demand_product_ids and rec.product_id not in rec.demand_product_ids:
                 raise ValidationError(
-                    _("Buffered product must be considered as demand."))
+                    _("Buffered product must be considered as demand.")
+                )
 
     @api.multi
     def _past_moves_domain(self, date_from, date_to, locations):
         if not self.demand_product_ids:
             return super()._past_moves_domain(date_from, date_to, locations)
-        return [('state', '=', 'done'), ('location_id', 'in', locations.ids),
-                ('location_dest_id', 'not in', locations.ids),
-                ('product_id', 'in', self.demand_product_ids.ids),
-                ('date', '>=', date_from),
-                ('date', '<=', date_to)]
+        return [
+            ("state", "=", "done"),
+            ("location_id", "in", locations.ids),
+            ("location_dest_id", "not in", locations.ids),
+            ("product_id", "in", self.demand_product_ids.ids),
+            ("date", ">=", date_from),
+            ("date", "<=", date_to),
+        ]
