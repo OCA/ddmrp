13b1582d53a99f28ff885d06bd722adca04e8900
Lois Rilo <lois.rilo@forgeflow.com>
2021-03-15 09:24:41 +0100

    [IMP] ddmrp_exclude_moves_adu_calc: black, isort, prettier

--- ddmrp_exclude_moves_adu_calc/models/stock_move.py
+++ ddmrp_exclude_moves_adu_calc/models/stock_move.py
@@ -2,7 +2,7 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
@@ -10,16 +10,18 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     exclude_from_adu = fields.Boolean(
-        string='Exclude this move from ADU calculation', copy=False,
+        string="Exclude this move from ADU calculation",
+        copy=False,
         help="If this flag is set this stock move will be excluded from ADU "
-             "calculation",
+        "calculation",
         index=True,
     )
 
     @api.multi
     def _toggle_exclude_from_adu(self):
         if not self.env.user.has_group("stock.group_stock_manager"):
-            raise UserError(_(
-                "Only inventory managers are allowed perform this action."))
+            raise UserError(
+                _("Only inventory managers are allowed perform this action.")
+            )
         for rec in self:
             rec.exclude_from_adu = not rec.exclude_from_adu
