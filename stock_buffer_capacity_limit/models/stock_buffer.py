9dcfed8a5607c6bc73667d50fee1673c56f82418
Lois Rilo <lois.rilo@forgeflow.com>
2021-03-31 09:53:09 +0200

    [13.0][FIX] stock_buffer_capacity_limit: only consider qty on buffered location, consider procure UoM and recommendation modifiers (qty multiple and min). Also add tests cases.

--- stock_buffer_capacity_limit/models/stock_buffer.py
+++ stock_buffer_capacity_limit/models/stock_buffer.py
@@ -2,6 +2,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, fields, models
+from odoo.tools import float_compare
 
 
 class StockBuffer(models.Model):
@@ -20,11 +21,38 @@ class StockBuffer(models.Model):
         res = super()._compute_procure_recommended_qty()
         for rec in self.filtered(lambda b: b.storage_capacity_limit > 0.0):
             recommendation_limit = max(
-                rec.storage_capacity_limit
-                - rec.product_id.qty_available
+                rec.storage_capacity_limit  # UoM of the product
+                - rec.product_location_qty
                 - rec.incoming_dlt_qty,
                 0,
             )
-            qty = min(rec.procure_recommended_qty, recommendation_limit)
-            rec.procure_recommended_qty = qty
+            if rec.procure_uom_id:
+                rounding = rec.procure_uom_id.rounding
+                limit_uom = rec.product_id.uom_id._compute_quantity(
+                    recommendation_limit, rec.procure_uom_id
+                )
+            else:
+                rounding = rec.product_uom.rounding
+                limit_uom = recommendation_limit
+
+            if (
+                float_compare(
+                    limit_uom, rec.procure_recommended_qty, precision_rounding=rounding
+                )
+                > 0
+            ):
+                # Recommendation is valid.
+                continue
+
+            new_recommendation = limit_uom
+            if (
+                float_compare(
+                    rec.procure_min_qty, limit_uom, precision_rounding=rounding
+                )
+                > 0
+            ):
+                new_recommendation = 0
+            elif rec.qty_multiple:
+                new_recommendation -= new_recommendation % rec.qty_multiple
+            rec.procure_recommended_qty = new_recommendation
         return res
