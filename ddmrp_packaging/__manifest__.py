a8a3756af6c9a77d8147320e13b1e0e2cea77cf9
davidborromeo <david.borromeo@forgeflow.com>
2021-05-21 12:44:54 +0200

    [MIG] ddmrp_packaging: Migration to 14.0

--- ddmrp_packaging/__manifest__.py
+++ ddmrp_packaging/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "DDMRP Packaging",
     "summary": "DDMRP integration with packaging",
-    "version": "13.0.1.6.0",
+    "version": "14.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/ddmrp",
     "category": "Warehouse Management",
