a894486da3422226feafd27e4af8a5d5932345e8
KV <khoivha@trobz.com>
2021-09-10 18:12:10 +0700

    [IMP] ddmrp_exclude_moves_adu_calc: black, isort, prettier

--- ddmrp_exclude_moves_adu_calc/__manifest__.py
+++ ddmrp_exclude_moves_adu_calc/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "DDMRP Exclude Moves ADU Calc",
     "summary": "Define additional rules to exclude certain moves from ADU "
     "calculation",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
     "maintainers": ["JordiBForgeFlow", "LoisRForgeFlow"],
     "website": "https://github.com/OCA/ddmrp",
