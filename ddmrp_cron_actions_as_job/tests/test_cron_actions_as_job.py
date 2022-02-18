8e043c08f38abbd48feb32227474345716b5a3b8
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-02-18 15:38:10 -0500

    [FIX] tests

--- ddmrp_cron_actions_as_job/tests/test_cron_actions_as_job.py
+++ ddmrp_cron_actions_as_job/tests/test_cron_actions_as_job.py
@@ -13,7 +13,11 @@ from odoo.addons.queue_job.tests.common import mock_with_delay
 class TestDdmrpCronActionsAsJob(TestDdmrpCommon):
     def test_cron_actions_delay_job(self):
         context = dict(self.env.context, auto_delay_ddmrp_cron_actions=True)
-        del context["test_queue_job_no_delay"]
+        context.update(
+            {
+                "test_queue_job_no_delay": False,
+            }
+        )
         buffer_a = self.buffer_a.with_context(**context)
 
         with mock_with_delay() as (delayable_cls, delayable):
@@ -35,7 +39,11 @@ class TestDdmrpCronActionsAsJob(TestDdmrpCommon):
 
     def test_calc_adu_delay_job(self):
         context = dict(self.env.context, auto_delay_ddmrp_calc_adu=True)
-        del context["test_queue_job_no_delay"]
+        context.update(
+            {
+                "test_queue_job_no_delay": False,
+            }
+        )
         buffer_a = self.buffer_a.with_context(**context)
 
         with mock_with_delay() as (delayable_cls, delayable):
