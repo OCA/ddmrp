7c1df7fb5ef9fcdb09c56f9e62ec2505d7ca937f
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-07-01 09:46:03 -0500

    [FIX] case when origin warehouse has 2 steps to delivery

--- ddmrp/__manifest__.py
+++ ddmrp/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "DDMRP",
     "summary": "Demand Driven Material Requirements Planning",
-    "version": "14.0.1.14.0",
+    "version": "14.0.1.14.1",
     "license": "LGPL-3",
     "development_status": "Beta",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
