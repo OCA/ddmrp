d8ab041e0fa9c926ec7d46638aa8428ccbbb5266
BernatPForgeFlow <bernat.puig@forgeflow.com>
2023-05-08 10:26:31 +0200

    [IMP] ddmrp_sale: Adapt view to new Stock Buffer view design

--- ddmrp_sale/models/stock_buffer.py
+++ ddmrp_sale/models/stock_buffer.py
@@ -8,6 +8,9 @@ class StockBuffer(models.Model):
     _inherit = "stock.buffer"
 
     can_serve_sales = fields.Boolean(compute="_compute_can_serve_sales", store=True,)
+    qualified_demand_sale_order_line_ids = fields.Many2many(
+        comodel_name="sale.order.line",
+    )
 
     def _get_sale_source_location(self):
         self.ensure_one()
@@ -44,18 +47,46 @@ class StockBuffer(models.Model):
         so_lines = self.env["sale.order.line"].search(domain)
         return so_lines
 
-    def _get_demand_by_days(self):
-        demand_by_days = super()._get_demand_by_days()
-        if self.can_serve_sales:
-            so_lines = self._search_sales_qualified_demand()
+    def _get_so_lines_by_days(self, so_lines):
+        so_lines_by_days = {}
+        sol_dates = [dt.date() for dt in so_lines.mapped("commitment_date")]
+        for d in sol_dates:
+            if not so_lines_by_days.get(d):
+                so_lines_by_days[d] = 0.0
+        for sol in so_lines:
+            date = sol.commitment_date.date()
+            so_lines_by_days[date] += sol.product_uom._compute_quantity(
+                sol.product_uom_qty, sol.product_id.uom_id
+            )
+        return so_lines_by_days
 
-            sol_dates = [dt.date() for dt in so_lines.mapped("commitment_date")]
-            for d in sol_dates:
-                if not demand_by_days.get(d):
-                    demand_by_days[d] = 0.0
-            for sol in so_lines:
-                date = sol.commitment_date.date()
-                demand_by_days[date] += sol.product_uom._compute_quantity(
-                    sol.product_uom_qty, sol.product_id.uom_id
-                )
-        return demand_by_days
+    def _calc_qualified_demand(self, current_date=False):
+        res = super()._calc_qualified_demand(current_date)
+        today = current_date or fields.date.today()
+        for rec in self:
+            if rec.can_serve_sales:
+                qualified_demand = 0.0
+                lines = rec._search_sales_qualified_demand()
+                so_lines_by_days = rec._get_so_lines_by_days(lines)
+                dates = list(set(so_lines_by_days.keys()))
+                for date in dates:
+                    if (
+                        so_lines_by_days.get(date, 0.0) >= rec.order_spike_threshold
+                        or date <= today
+                    ):
+                        qualified_demand += so_lines_by_days.get(date, 0.0)
+                    else:
+                        lines = lines.filtered(
+                            lambda x: x.commitment_date.date() != date
+                        )
+                rec.qualified_demand += qualified_demand
+                rec.qualified_demand_sale_order_line_ids = lines
+        return res
+
+    def action_view_qualified_demand_so_lines(self):
+        lines = self.qualified_demand_sale_order_line_ids
+        action = self.env.ref("sale.action_quotations")
+        result = action.read()[0]
+        result["context"] = {}
+        result["domain"] = [("id", "in", lines.mapped("order_id.id"))]
+        return result
