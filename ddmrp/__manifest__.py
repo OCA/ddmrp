9cae277212bcd643ca189454ddfee8f735a8fa5d
Lois Rilo <lois.rilo@forgeflow.com>
2023-05-15 12:07:56 +0200

    [UPD] force new pypi release

--- ddmrp/__manifest__.py
+++ ddmrp/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "DDMRP",
     "summary": "Demand Driven Material Requirements Planning",
-    "version": "13.0.1.29.0",
+    "version": "13.0.1.29.1",
     "license": "LGPL-3",
     "development_status": "Beta",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
