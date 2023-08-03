1cff4da98f61f399652b4890d3d58b08e0a9eea6
BernatPForgeFlow <bernat.puig@forgeflow.com>
2023-08-03 09:29:45 +0200

    [IMP] ddmrp: Autocalculate buffer in Manufacturing Orders
    Perform a calculation by level in which the location level is first searched and if not found it is searched at the warehouse level.

--- ddmrp/models/mrp_production.py
+++ ddmrp/models/mrp_production.py
@@ -27,8 +27,46 @@ class MrpProduction(models.Model):
     @api.model
     def create(self, vals):
         record = super(MrpProduction, self).create(vals)
+        record._find_buffer_link()
         record._calc_execution_priority()
         return record
+
+    def write(self, vals):
+        res = super().write(vals)
+        if any(
+            f in vals
+            for f in ("product_id", "picking_type_id", "location_dest_id", "company_id")
+        ):
+            self._find_buffer_link()
+        return res
+
+    def _get_domain_buffer_link(self, warehouse_level=False):
+        self.ensure_one()
+        domain = [
+            ("product_id", "=", self.product_id.id),
+            ("company_id", "=", self.company_id.id),
+            ("buffer_profile_id.item_type", "=", "manufactured"),
+        ]
+        if not warehouse_level:
+            locations = self.env["stock.location"].search(
+                [("id", "child_of", [self.location_dest_id.id])]
+            )
+            domain += [("location_id", "in", locations.ids)]
+        else:
+            domain += [("warehouse_id", "=", self.picking_type_id.warehouse_id.id)]
+        return domain
+
+    def _find_buffer_link(self):
+        buffer_model = self.env["stock.buffer"]
+        for rec in self:
+            domain = rec._get_domain_buffer_link()
+            buffer = buffer_model.search(domain, limit=1)
+            if not buffer:
+                domain = rec._get_domain_buffer_link(warehouse_level=True)
+                buffer = buffer_model.search(domain, limit=1)
+            rec.buffer_id = buffer
+            if buffer:
+                rec._calc_execution_priority()
 
     def _calc_execution_priority(self):
         """Technical note: this method cannot be decorated with api.depends,
