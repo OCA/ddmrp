a6557af9fae97f9df9903bc896deee52dcf8e9c8
BernatPForgeFlow <bernat.puig@forgeflow.com>
2023-09-26 09:51:02 +0200

    [IMP] stock_buffer_route: Add alternative item type in buffer
    Considering that the 'Route' field will be used to define a different way to procure, a field called 'Alternative Item Type' is added in the Stock Buffer.
    This will help improve the search with the filters and enhance the calculations by taking the alternative route into account.

--- stock_buffer_route/wizards/make_procurement_buffer.py
+++ stock_buffer_route/wizards/make_procurement_buffer.py
@@ -3,26 +3,26 @@
 
 from odoo import api, fields, models
 
+_ITEM_TYPES = [
+    ("manufactured", "Manufactured"),
+    ("purchased", "Purchased"),
+    ("distributed", "Distributed"),
+]
+
 
 class MakeProcurementBuffer(models.TransientModel):
     _inherit = "make.procurement.buffer"
-
-    partner_id = fields.Many2one(
-        comodel_name="res.partner",
-        string="Vendor",
-        help="If set, will be used as preferred vendor for purchase routes.",
-    )
-    item_ids = fields.One2many(
-        comodel_name="make.procurement.buffer.item",
-        inverse_name="wiz_id",
-        string="Items",
-    )
 
     @api.model
     def _prepare_item(self, buffer, qty_override=None):
         res = super(MakeProcurementBuffer, self)._prepare_item(buffer, qty_override)
         res["route_ids"] = buffer.route_ids.ids
         res["route_id"] = buffer.route_id.id
+        context = {
+            "route_id": buffer.route_id,
+            "item_type_alternative": buffer.item_type_alternative,
+        }
+        res["date_planned"] = buffer.with_context(**context)._get_date_planned()
         return res
 
 
@@ -37,6 +37,35 @@ class MakeProcurementBufferItem(models.TransientModel):
         "stock.location.route",
         domain="[('id', 'in', route_ids)]",
     )
+    item_type_alternative = fields.Selection(
+        string="Alternative Item Type",
+        selection=_ITEM_TYPES,
+        compute="_compute_item_type_alternative",
+        store=True,
+    )
+
+    @api.onchange("route_id")
+    def _onchange_route_id(self):
+        for rec in self:
+            context = {
+                "route_id": rec.route_id,
+                "item_type_alternative": rec.item_type_alternative,
+            }
+            rec.date_planned = rec.buffer_id.with_context(**context)._get_date_planned()
+
+    @api.depends("route_id", "route_id.rule_ids", "route_id.rule_ids.action")
+    def _compute_item_type_alternative(self):
+        for rec in self:
+            rec.item_type_alternative = ""
+            if rec.route_id:
+                if "buy" in rec.route_id.mapped("rule_ids.action"):
+                    rec.item_type_alternative = "purchased"
+                elif "manufacture" in rec.route_id.mapped("rule_ids.action"):
+                    rec.item_type_alternative = "manufactured"
+                elif "pull" in rec.route_id.mapped(
+                    "rule_ids.action"
+                ) or "pull_push" in rec.route_id.mapped("rule_ids.action"):
+                    rec.item_type_alternative = "distributed"
 
     def _prepare_values_make_procurement(self):
         values = super()._prepare_values_make_procurement()
