ec77291af5e45e6820bf5d59b3987b6694996db7
BernatPForgeFlow <bernat.puig@forgeflow.com>
2023-12-01 14:23:11 +0100

    [FIX] ddmrp: Compute chart fields in list view only when click
    If we compute all the charts when opening the list view it strongly affects performance. Then, as a solution, we only compute the chart when it is clicked.

--- ddmrp/static/src/widgets/stock_buffer_info.esm.js
+++ ddmrp/static/src/widgets/stock_buffer_info.esm.js
@@ -1,20 +1,19 @@
 /** @odoo-module **/
 
 import {FloatField} from "@web/views/fields/float/float_field";
-import {formatDateTime} from "@web/core/l10n/dates";
 import {loadBundle} from "@web/core/assets";
-import {localization} from "@web/core/l10n/localization";
 import {registry} from "@web/core/registry";
-import {usePopover} from "@web/core/popover/popover_hook";
 import {useService} from "@web/core/utils/hooks";
+import {useUniquePopover} from "@web/core/model_field_selector/unique_popover_hook";
 
-const {Component, EventBus, onWillRender, onWillStart, markup} = owl;
+const {Component, markup, onWillStart} = owl;
 
 export class StockBufferPopover extends Component {
     setup() {
         this.actionService = useService("action");
-        onWillStart(() =>
-            loadBundle({
+        this.orm = useService("orm");
+        onWillStart(async () => {
+            await loadBundle({
                 jsLibs: [
                     "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-3.1.1.min.js",
                     "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-api-3.1.1.min.js",
@@ -23,36 +22,22 @@ export class StockBufferPopover extends Component {
                     "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-mathjax-3.1.1.min.js",
                     "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-gl-3.1.1.min.js",
                 ],
-            })
-        );
+            });
+            this.bokeh_chart = await this.orm.read(
+                this.props.record.resModel,
+                [this.props.record.resId],
+                [this.props.field]
+            );
+        });
     }
     get json_value() {
         try {
-            var value = JSON.parse(this.props.record.data[this.props.field]);
+            var value = JSON.parse(this.bokeh_chart[0][this.props.field]);
             value.div = markup(value.div.trim());
             return value;
         } catch (error) {
             return {};
         }
-    }
-
-    openForecast() {
-        this.actionService.doAction(
-            "stock.stock_replenishment_product_product_action",
-            {
-                additionalContext: {
-                    active_model: "product.product",
-                    active_id: this.props.record.data.product_id[0],
-                    warehouse:
-                        this.props.record.data.warehouse_id &&
-                        this.props.record.data.warehouse_id[0],
-                    move_to_match_ids: this.props.record.data.move_ids.records.map(
-                        (record) => record.data.id
-                    ),
-                    sale_line_to_match_id: this.props.record.data.id,
-                },
-            }
-        );
     }
 }
 StockBufferPopover.template = "ddmrp.StockBufferPopover";
@@ -60,13 +45,7 @@ StockBufferPopover.template = "ddmrp.StockBufferPopover";
 export class StockBufferInfoWidget extends FloatField {
     setup() {
         super.setup();
-        this.bus = new EventBus();
-        this.popover = usePopover();
-        this.closePopover = null;
-        this.calcData = {};
-        onWillRender(() => {
-            this.initCalcData();
-        });
+        this.popover = useUniquePopover();
     }
 
     get classFromDecoration() {
@@ -78,71 +57,22 @@ export class StockBufferInfoWidget extends FloatField {
         return "";
     }
 
-    initCalcData() {
-        // Calculate data not in record
-        const {data} = this.props.record;
-        if (data.scheduled_date) {
-            // TODO: might need some round_decimals to avoid errors
-            if (data.state === "sale") {
-                this.calcData.will_be_fulfilled =
-                    data.free_qty_today >= data.qty_to_deliver;
-            } else {
-                this.calcData.will_be_fulfilled =
-                    data.virtual_available_at_date >= data.qty_to_deliver;
-            }
-            this.calcData.will_be_late =
-                data.forecast_expected_date &&
-                data.forecast_expected_date > data.scheduled_date;
-            if (["draft", "sent"].includes(data.state)) {
-                // Moves aren't created yet, then the forecasted is only based on virtual_available of quant
-                this.calcData.forecasted_issue =
-                    !this.calcData.will_be_fulfilled && !data.is_mto;
-            } else {
-                // Moves are created, using the forecasted data of related moves
-                this.calcData.forecasted_issue =
-                    !this.calcData.will_be_fulfilled || this.calcData.will_be_late;
-            }
-        }
-    }
-
-    updateCalcData() {
-        // Popup specific data
-        const {data} = this.props.record;
-        if (!data.scheduled_date) {
-            return;
-        }
-        this.calcData.delivery_date = formatDateTime(data.scheduled_date, {
-            format: localization.dateFormat,
-        });
-        if (data.forecast_expected_date) {
-            this.calcData.forecast_expected_date_str = formatDateTime(
-                data.forecast_expected_date,
-                {format: localization.dateFormat}
-            );
-        }
-    }
-
     showPopup(ev) {
         ev.stopPropagation();
         ev.preventDefault();
-        this.updateCalcData();
-        if (!this.closePopover) {
-            this.closePopover = this.popover.add(
-                ev.currentTarget,
-                this.constructor.components.Popover,
-                {
-                    bus: this.bus,
-                    record: this.props.record,
-                    calcData: this.calcData,
-                    field: this.props.field,
-                    color_from: this.props.color_from,
-                },
-                {
-                    position: "right",
-                }
-            );
-            this.bus.addEventListener("close-popover", this.closePopover);
-        }
+        this.popover.add(
+            ev.currentTarget,
+            this.constructor.components.Popover,
+            {
+                bus: this.bus,
+                record: this.props.record,
+                field: this.props.field,
+                color_from: this.props.color_from,
+            },
+            {
+                position: "right",
+            }
+        );
     }
 }
 
