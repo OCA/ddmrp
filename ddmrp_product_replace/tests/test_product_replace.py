a6a3a2ac296e12e5542a4463e549a8f1a5c420c8
Lois Rilo <lois.rilo@forgeflow.com>
2023-12-04 16:16:22 +0100

    [MIG] ddmrp_product_replace: Migration to 16.0

--- ddmrp_product_replace/tests/test_product_replace.py
+++ ddmrp_product_replace/tests/test_product_replace.py
@@ -128,7 +128,7 @@ class TestDDMRPProductReplace(TestDdmrpCommon):
         self.assertEqual(old_onhand, new_buffer.product_location_qty_available_not_res)
         self.assertEqual(old_incoming_dlt_qty, new_buffer.incoming_dlt_qty)
         self.assertEqual(old_incoming_qty, new_buffer.incoming_total_qty)
-        new_buffer.invalidate_cache()
+        new_buffer.invalidate_recordset()
         new_buffer.use_replacement_for_buffer_status = False
         new_buffer.cron_actions()
         self.assertNotEqual(
