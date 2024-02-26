d04059c742d4a672f241c7d43961a760fdee6350
DavidJForgeFlow <david.jimenez@forgeflow.com>
2024-02-26 13:57:57 +0100

    [ADD] ddmrp_exclude_moves_adu_calc_sales

---
+++ ddmrp_exclude_moves_adu_calc_sales/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "DDMRP Exclude Moves ADU Calc Sales",
+    "summary": "DDMRP Exclude Moves ADU Calc integration with Sales app.",
+    "version": "16.0.1.0.0",
+    "development_status": "Beta",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "maintainers": ["DavidJForgeFlow"],
+    "website": "https://github.com/OCA/ddmrp",
+    "category": "Warehouse Management",
+    "depends": ["sale", "ddmrp_exclude_moves_adu_calc"],
+    "data": ["views/sale_order_view.xml"],
+    "license": "LGPL-3",
+    "installable": True,
+}
