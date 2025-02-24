a1722d55fd6499e099affc9e4efdc7ce2e3c6a10
ThiagoMForgeFlow <thiago.mulero@forgeflow.com>
2025-02-24 10:23:25 +0100

    [MIG] ddmrp_warning: Migration to 18.0

--- ddmrp_warning/__manifest__.py
+++ ddmrp_warning/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "DDMRP Warning",
-    "version": "17.0.1.1.0",
+    "version": "18.0.1.0.0",
     "summary": "Adds configuration warnings on stock buffers.",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "development_status": "Production/Stable",
