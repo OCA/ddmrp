01d27bbf75239d0d89de94bb100ef21471504b31
Don Kendall <dkendall@ledoweb.com>
2026-06-15 12:12:27 -0400

    [MIG] ddmrp_purchase_hide_onhand_status: Migration to 19.0
    Signed-off-by: Don Kendall <dkendall@ledoweb.com>

--- ddmrp_purchase_hide_onhand_status/tests/test_ddmrp_purchase_hide_onhand_status.py
+++ ddmrp_purchase_hide_onhand_status/tests/test_ddmrp_purchase_hide_onhand_status.py
@@ -8,7 +8,22 @@ class TestDDMRPPurchaseHideOnhandStatus(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.po = cls.env.ref("purchase.purchase_order_1")
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        vendor = cls.env["res.partner"].create({"name": "Test Vendor"})
+        product = cls.env["product.product"].create(
+            {
+                "name": "Test Product",
+                "is_storable": True,
+            }
+        )
+        cls.po = cls.env["purchase.order"].create({"partner_id": vendor.id})
+        cls.env["purchase.order.line"].create(
+            {
+                "order_id": cls.po.id,
+                "product_id": product.id,
+                "product_qty": 5.0,
+            }
+        )
 
     def test_ddmrp_purchase_hide_onhand_status(self):
         value = self.po.action_ddmrp_line_details()
